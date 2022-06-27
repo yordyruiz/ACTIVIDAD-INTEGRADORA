758b5d325805b9e4df6c5277f3ba4b6a59395d76
yordyruiz <yordyruiz11@gmail.com>
2022-06-26 21:25:10 -0300

    Funcion SUMA

--- PROGRAMACION INICIAL.py
+++ PROGRAMACION INICIAL.py
@@ -1,1 +1,21 @@
+#lista de numeros 
+num_1 = int(input("Ingresa el primer número: "))
+num_2 = int(input("Ingresa el segundo número: "))
+num_3 = int(input("Ingresa el tercer número: "))
+num_4 = int(input("Ingresa el cuarto número: "))
+num_5 = int(input("Ingresa el quinto número: "))
 
+lista = [num_1, num_2, num_3, num_4, num_5]
+total = 0
+
+#Funcion suma 
+lista = [num_1, num_2, num_3, num_4, num_5]
+total = 0
+
+for i in lista:
+    total += i
+
+print("El resultado de la suma de todos los elementos es" + total)
+
+#yordy ruiz
+#Aula 1
