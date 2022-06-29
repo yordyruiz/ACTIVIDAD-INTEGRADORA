a642d4f4e0fef84d85a6be89915dfe0cfc1430d5
ElianY7 <elian.yague@gmail.com>
2022-06-29 20:46:26 -0300

    Agregue las opciones y arreglos

--- Minimo.py
+++ Minimo.py
@@ -1,12 +1,9 @@
-from xml.dom import minicompat
-
-
 def minimo (lista):
    
    minimo=0
 
-    for i in range (len(lista)):
-     if lista[i]>minimo:
-       minimo=lista[i]
+   for i in range (len(lista)):
+      if lista[i]<minimo:
+         minimo=lista[i]
 
-    print("El mayor número es:", minimo)
+   print("El mayor número es:", minimo)
--- PROGRAMACION INICIAL.py
+++ PROGRAMACION INICIAL.py
@@ -1,5 +1,7 @@
 #módulos
+from sum import suma
 from maximo import mayor
+from Minimo import minimo
 from promedio import calcular_promedio  
 
 #lista de numeros 
@@ -7,9 +9,49 @@ lista = []
 
 for i in range(1,6):
     lista += [int(input("Ingresa el " + str(i) + ".º número entero: "))]
-calcular_promedio(lista)
 
 
+#Seleccion de operacion
+
+opcion=int(input("""
+----Operaciones disponibles----
+
+1) Suma
+2) Promedio
+3) Máximo
+4) Mínimo
+Seleccione un número: """))
+
+
+while True:
+
+  if opcion==1:
+
+    print("Eligió suma")
+    suma(lista)
+    break
+
+  elif opcion==2:
+
+    print("Eligió promedio")
+    calcular_promedio(lista)
+    break
+
+  elif opcion==3:
+
+    print("Eligió maximo")
+    mayor(lista)
+    break
+
+  elif opcion==4:
+
+    print("Eligió minimo")
+    minimo(lista)
+    break
+
+  else:
+
+    opcion=int(input(("Número incorrecto, ingrese nuevamente: ")))
 
  
 
