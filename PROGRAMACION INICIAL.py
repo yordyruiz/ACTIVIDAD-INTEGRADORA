f978ce4feb6c6a5d3b95c4ebfeaa5a50af1e01b3
Maga12 <maga.an@gmail.com>
2022-06-29 20:27:13 -0300

    "Se agrega sum"

--- PROGRAMACION INICIAL.py
+++ PROGRAMACION INICIAL.py
@@ -9,15 +9,7 @@ for i in range(1,6):
     lista += [int(input("Ingresa el " + str(i) + ".º número entero: "))]
 calcular_promedio(lista)
 
-#Funcion suma 
-def suma(lista):
 
-    total = 0
-
-    for i in lista:
-        total += i
-
-    print("El resultado de la suma de todos los elementos es", total)
 
  
 
---
+++ sum.py
@@ -0,0 +1,8 @@
+def suma(lista):
+
+    total = 0
+
+    for i in lista:
+        total += i
+
+    print("El resultado de la suma de todos los elementos es", total)
